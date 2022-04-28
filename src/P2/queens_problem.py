29195be5c2e752473f3984cacc72e283eea3cc7e
JustACommentator <t.glasmacher@aol.com>
2022-04-28 18:41:45 +0200

    added some stuff

--- src/P2/queens_problem.py
+++ src/P2/queens_problem.py
@@ -2,18 +2,30 @@ import random
 
 
 class QueensProblem:
+
+
     def __init__(self):
         self.board = [["+" for i in range(8)] for k in range(8)]
 
+
     def setBoard(self, queenString):
         for i in range(8):
-            self.board[i][int(queenString[i])-1] = "Q"
+            self.board[i][int(queenString[i]) - 1] = "Q"
+
 
     def randomizeBoard(self):
         queenString = ""
         for i in range(8):
             queenString += str(random.randint(1, 8))
         self.setBoard(queenString)
+
+
+    def generateQueenString(self):
+        queenString = ""
+        for i in range(8):
+            queenString += str(self.board[i].index("Q") + 1)
+        return queenString
+
 
     def printBoard(self):
         for i in range(len(self.board)):
@@ -22,7 +34,31 @@ class QueensProblem:
             print("")
 
 
+def fitnessScore(queenString):  # using number of non-attacking pairs of queens as fitness score. Optimal: 28
+    score = 0
+
+    for x1 in range(8):
+        y = int(queenString[x1])
+
+        for x2 in range(8):
+
+            if x2 == x1:
+                continue
+            if int(queenString[x2]) == y:
+                continue
+            if x2 + int(queenString[x2]) == x1 + y:
+                continue
+            if x2 - int(queenString[x2]) == x1 - y:
+                continue
+            score += 1
+
+    return score / 2  # each queen is counted twice per pair
+
 
 Q = QueensProblem()
-Q.randomizeBoard()
+
+Q.setBoard("83742516")
+
 Q.printBoard()
+
+print(fitnessScore(Q.generateQueenString()))
