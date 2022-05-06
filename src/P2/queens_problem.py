d681c6390d6540d41b8738994157b8b11f988447
JustACommentator <t.glasmacher@aol.com>
2022-05-07 01:58:12 +0200

    Implemented genetic algorithm. It's slow, maybe check for optimizations

--- src/P2/queens_problem.py
+++ src/P2/queens_problem.py
@@ -1,4 +1,14 @@
 import random
+import numpy
+
+# one in X cases will mutate
+MUTATION_CHANCE = 25
+
+# iterations til end
+ITERATION_COUNT = 100
+
+# size of Population
+POPULATION_SIZE = 100
 
 
 class QueensProblem:
@@ -6,15 +16,15 @@ class QueensProblem:
     def __init__(self):
         self.queenString = ""
 
-    def setBoard(self, queenString):
+    def set(self, queenString):
         self.queenString = queenString
 
-    def randomizeBoard(self):
+    def randomize(self):
         self.queenString = ""
         for i in range(8):
             self.queenString += str(random.randint(1, 8))
 
-    def printBoard(self):
+    def print(self):
         for i in range(8):
             for k in range(8):
                 if int(self.queenString[k]) == i + 1:
@@ -23,29 +33,87 @@ class QueensProblem:
                     print(".  ", end="")
             print("")
 
-    def fitnessScore(self):  # using number of non-attacking pairs of queens as fitness score. Optimal: 28
-        score = 0
 
-        for x1 in range(8):
-            y = int(self.queenString[x1])
+def fitnessScore(chromosome):  # using number of non-attacking pairs of queens as fitness score. Optimal: 28
+    score = 0
 
-            for x2 in range(8):
+    for x1 in range(8):
+        y = int(chromosome[x1])
 
-                if x2 == x1:
-                    continue
-                if int(self.queenString[x2]) == y:
-                    continue
-                if x2 + int(self.queenString[x2]) == x1 + y:
-                    continue
-                if x2 - int(self.queenString[x2]) == x1 - y:
-                    continue
-                score += 1
+        for x2 in range(8):
 
-        return int(score / 2)  # each queen is counted twice per pair
+            if x2 == x1:
+                continue
+            if int(chromosome[x2]) == y:
+                continue
+            if x2 + int(chromosome[x2]) == x1 + y:
+                continue
+            if x2 - int(chromosome[x2]) == x1 - y:
+                continue
+            score += 1
+
+    return int(score / 2)  # each queen is counted twice per pair
 
 
+def geneticAlgorithm(population):
+    fitnesses = []
+    for chromosome in population:
+        fitnesses.append(fitnessScore(chromosome))
+
+    newPopulation = []
+    for i in range(len(population)):
+        parent1 = selectParents(population, fitnesses)
+        parent2 = selectParents(population, fitnesses)
+        child = reproduce(parent1, parent2)
+        newPopulation.append(child)
+    return newPopulation
+
+
+def selectParents(population, fitnesses):
+    totalFitness = sum(fitnesses)
+    chromosomeProbabilities = [fitnessScore(chromosome) / totalFitness for chromosome in population]
+    return numpy.random.choice(population, p=chromosomeProbabilities)
+
+
+def reproduce(x, y):  # crossover not guaranteed. For guaranteed crossover: Change c range
+    n = len(x)
+    c = random.randint(0, n)
+    child = ""
+    child += x[0:c]
+    child += y[c:n + 1]
+    if random.randint(1, MUTATION_CHANCE) == 1:
+        child = mutate(child)
+    return child
+
+
+def mutate(child):
+    n = random.randint(0, 7)
+    child = child[:n] + str(random.randint(1, 8)) + child[n + 1:]
+    return child
+
+
+def populate():
+    Q = QueensProblem()
+    population = []
+    for i in range(POPULATION_SIZE):
+        Q.randomize()
+        population.append(Q.queenString)
+
+    return population
+
+
+population= populate()
+for i in range(ITERATION_COUNT):
+    print(f"Generation {i + 1}:")
+    population = geneticAlgorithm(population)
+    fitnesses = []
+    for chromosome in population:
+        fitnesses.append(fitnessScore(chromosome))
+    print(f"Best specimen: {population[fitnesses.index(max(fitnesses))]}. Fitness score: {max(fitnesses)}. Average: {sum(fitnesses)/len(fitnesses)}")
+
+print(f"Best specimen in final generation after {ITERATION_COUNT} iterations with {POPULATION_SIZE} specimen per generation:")
+best = population[fitnesses.index(max(fitnesses))]
+print(f"Fitness score: {max(fitnesses)}")
 Q = QueensProblem()
-
-Q.randomizeBoard()
-
-Q.printBoard()
+Q.set(best)
+Q.print()
