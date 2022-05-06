39f39d0e4fe00617f35fb70aeef9a435c8ce6793
JustACommentator <t.glasmacher@aol.com>
2022-05-06 17:25:21 +0200

    minor cleanup, added fitnessScore to QueensProblem class

--- src/P2/queens_problem.py
+++ src/P2/queens_problem.py
@@ -3,15 +3,15 @@ import random
 
 class QueensProblem:
 
-
     def __init__(self):
         self.board = [["+" for i in range(8)] for k in range(8)]
-
+        self.queenString = ""
 
     def setBoard(self, queenString):
         for i in range(8):
             self.board[i][int(queenString[i]) - 1] = "Q"
-
+        for i in range(8):
+            self.queenString += str(self.board[i].index("Q") + 1)
 
     def randomizeBoard(self):
         queenString = ""
@@ -19,46 +19,37 @@ class QueensProblem:
             queenString += str(random.randint(1, 8))
         self.setBoard(queenString)
 
-
-    def generateQueenString(self):
-        queenString = ""
-        for i in range(8):
-            queenString += str(self.board[i].index("Q") + 1)
-        return queenString
-
-
     def printBoard(self):
         for i in range(len(self.board)):
             for k in range(len(self.board[0])):
                 print(self.board[k][i] + "  ", end="")
             print("")
 
+    def fitnessScore(self):  # using number of non-attacking pairs of queens as fitness score. Optimal: 28
+        score = 0
 
-def fitnessScore(queenString):  # using number of non-attacking pairs of queens as fitness score. Optimal: 28
-    score = 0
+        for x1 in range(8):
+            y = int(self.queenString[x1])
 
-    for x1 in range(8):
-        y = int(queenString[x1])
+            for x2 in range(8):
 
-        for x2 in range(8):
+                if x2 == x1:
+                    continue
+                if int(self.queenString[x2]) == y:
+                    continue
+                if x2 + int(self.queenString[x2]) == x1 + y:
+                    continue
+                if x2 - int(self.queenString[x2]) == x1 - y:
+                    continue
+                score += 1
 
-            if x2 == x1:
-                continue
-            if int(queenString[x2]) == y:
-                continue
-            if x2 + int(queenString[x2]) == x1 + y:
-                continue
-            if x2 - int(queenString[x2]) == x1 - y:
-                continue
-            score += 1
-
-    return score / 2  # each queen is counted twice per pair
+        return score / 2  # each queen is counted twice per pair
 
 
 Q = QueensProblem()
 
-Q.setBoard("83742516")
+Q.randomizeBoard()
 
 Q.printBoard()
 
-print(fitnessScore(Q.generateQueenString()))
+print(Q.fitnessScore())
